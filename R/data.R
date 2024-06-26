32ee454ced8f17f3b4496425cd25103daeb69205
Dee Velazquez <88875666+dvelazq@users.noreply.github.com>
2024-06-26 12:37:22 -0400

    Add data file

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Deconvolved cell-type proportion matrix of spatial transcriptomic data from mouse olfactory bulb
+#'
+#' @format A data frame with 260 rows and 8 columns, that contains the eight deconvolved cell-types proportions for each 260 spots/pixels in a spatial transcriptomics experiment performed on mouse olfactory bulb tissue.
+#'
+#' @source \url{https://science.sciencemag.org/content/353/6294/78}
+#'
+#' @usage data(deconProp)
+"deconProp"
+
+#' Positions for spots from mouse olfactory bulb tissue
+#'
+#' @format A data frame with 260 rows and 2 columns, that contains the x and y-coordinates for each 260 spots/pixels on a mouse olfactory bulb tissue slide.
+#'
+#' @source \url{https://science.sciencemag.org/content/353/6294/78}
+#'
+#' @usage data(pos)
+"pos"
